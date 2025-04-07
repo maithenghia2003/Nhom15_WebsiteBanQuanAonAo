655ce0ea6c3bd2e91003dfbb4337312bfa98eee9
vokimlam <lambeomap7777@gmail.com>
2025-04-07 20:48:49 +0700

    Add-Cart

---
+++ Customer/src/main/java/com/example/Customer/controller/ShoppingCartController.java
@@ -0,0 +1,66 @@
+package com.example.Customer.controller;
+
+
+import com.example.Customer.dto.ProductDto;
+import com.example.Customer.model.Customer;
+import com.example.Customer.model.ShoppingCart;
+import com.example.Customer.service.CustomerService;
+import com.example.Customer.service.ProductService;
+import com.example.Customer.service.ShoppingCartService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class ShoppingCartController {
+    private final ShoppingCartService cartService;
+    private final ProductService productService;
+    private final CustomerService customerService;
+
+    @GetMapping("/cart")
+    public String cart(Model model, Principal principal, HttpSession session) {
+        if (principal == null) {
+            return "redirect:/login";
+        }
+        Customer customer = customerService.findByUsername(principal.getName());
+        ShoppingCart cart = customer.getCart();
+        if (cart == null) {
+            model.addAttribute("check");
+
+        }
+        if (cart != null) {
+            model.addAttribute("grandTotal", cart.getTotalPrice());
+        }
+        model.addAttribute("shoppingCart", cart);
+        model.addAttribute("title", "Cart");
+        session.setAttribute("totalItems", cart.getTotalItems());
+        return "cart";
+
+
+    }
+
+    @PostMapping("/add-to-cart")
+    public String addItemToCart(@RequestParam("id") Long id,
+                                @RequestParam(value = "quantity", required = false, defaultValue = "1") int quantity,
+                                HttpServletRequest request,
+                                Model model,
+                                Principal principal,
+                                HttpSession session) {
+
+
+        ProductDto productDto = productService.getById(id);
+        if (principal == null) {
+            return "redirect:/login";
+        }
+        String username = principal.getName();
+        ShoppingCart shoppingCart = cartService.addItemToCart(productDto, quantity, username);
+        session.setAttribute("totalItems", shoppingCart.getTotalItems());
+        model.addAttribute("shoppingCart", shoppingCart);
+        return "redirect:" + request.getHeader("Referer");
+    }
