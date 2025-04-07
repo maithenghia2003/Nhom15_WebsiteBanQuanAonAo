e05fd03982689d5e9f0b53c881e64a43953914e8
maithenghia2003 <maithenghia2506@gmail.com>
2025-04-07 23:31:16 +0700

    SCRUM-43 <message>

---
+++ Customer/src/main/java/com/example/Customer/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.Customer.controller;
+
+
+import com.example.Customer.dto.CustomerDto;
+import com.example.Customer.model.Customer;
+import com.example.Customer.service.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+    private final CustomerService customerService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("title", "Login Page");
+        model.addAttribute("page", "Home");
+        return "login";
+    }
+
+
+
+}
