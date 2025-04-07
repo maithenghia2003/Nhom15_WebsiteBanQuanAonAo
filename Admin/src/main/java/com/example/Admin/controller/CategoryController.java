ed5de93e43e62b4b94a1fee2e49b2ca21b6b0cc5
hiu1412 <144113591+hiu1412@users.noreply.github.com>
2025-04-07 22:27:11 +0700

    SCRUM-34 <message>

---
+++ Admin/src/main/java/com/example/Admin/controller/CategoryController.java
@@ -0,0 +1,57 @@
+package com.example.Admin.controller;
+
+
+import com.example.Admin.model.Category;
+import com.example.Admin.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/save-category")
+    public String save(@ModelAttribute("categoryNew") Category category, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            categoryService.save(category);
+            model.addAttribute("categoryNew", category);
+            redirectAttributes.addFlashAttribute("success", "Add successfully!");
+        } catch (DataIntegrityViolationException e1) {
+            e1.printStackTrace();
+            redirectAttributes.addFlashAttribute("error", "Duplicate name of category, please check again!");
+        } catch (Exception e2) {
+            e2.printStackTrace();
+            model.addAttribute("categoryNew", category);
+            redirectAttributes.addFlashAttribute("error",
+                    "Error server");
+        }
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/update-category")
+    public String update(Category category, RedirectAttributes redirectAttributes) {
+        try {
+            categoryService.update(category);
+            redirectAttributes.addFlashAttribute("success", "Update successfully!");
+        } catch (DataIntegrityViolationException e1) {
+            e1.printStackTrace();
+            redirectAttributes.addFlashAttribute("error", "Duplicate name of category, please check again!");
+        } catch (Exception e2) {
+            e2.printStackTrace();
+            redirectAttributes.addFlashAttribute("error", "Error from server or duplicate name of category, please check again!");
+        }
+        return "redirect:/categories";
+    }
+
